8bb5517af124d0b486c0c8e270caf8d724dd2676
Johnson Ubaezuonu <jaycodist@gmail.com>
2023-12-08 23:56:14 +0100

    added sentry for monitoring

--- next.config.js
+++ next.config.js
@@ -1,17 +1,53 @@
-module.exports = {
-  reactStrictMode: true,
-  images: {
-    domains: ["storage.googleapis.com"],
-    unoptimized: true
-  },
-  trailingSlash: true,
-  exportPathMap() {
-    return {
-      "/404": { page: "/404" }
-    };
-  }
-  // i18n: {
-  //   locales: ["en"],
-  //   defaultLocale: "en"
-  // }
+import { withSentryConfig } from "@sentry/nextjs";
+
+export const reactStrictMode = true;
+export const images = {
+  domains: ["storage.googleapis.com"],
+  unoptimized: true
 };
+export const trailingSlash = true;
+export function exportPathMap() {
+  return {
+    "/404": { page: "/404" }
+  };
+}
+
+// Injected content via Sentry wizard below
+
+export default withSentryConfig(
+  module.exports,
+  {
+    // For all available options, see:
+    // https://github.com/getsentry/sentry-webpack-plugin#options
+
+    // Suppresses source map uploading logs during build
+    silent: true,
+    org: "jaycodist",
+    project: "javascript-nextjs"
+  },
+  {
+    // For all available options, see:
+    // https://docs.sentry.io/platforms/javascript/guides/nextjs/manual-setup/
+
+    // Upload a larger set of source maps for prettier stack traces (increases build time)
+    widenClientFileUpload: true,
+
+    // Transpiles SDK to be compatible with IE11 (increases bundle size)
+    transpileClientSDK: true,
+
+    // Routes browser requests to Sentry through a Next.js rewrite to circumvent ad-blockers (increases server load)
+    tunnelRoute: "/monitoring",
+
+    // Hides source maps from generated client bundles
+    hideSourceMaps: true,
+
+    // Automatically tree-shake Sentry logger statements to reduce bundle size
+    disableLogger: true,
+
+    // Enables automatic instrumentation of Vercel Cron Monitors.
+    // See the following for more information:
+    // https://docs.sentry.io/product/crons/
+    // https://vercel.com/docs/cron-jobs
+    automaticVercelMonitors: true
+  }
+);
---
+++ sentry.server.config.ts
@@ -0,0 +1,16 @@
+// This file configures the initialization of Sentry on the server.
+// The config you add here will be used whenever the server handles a request.
+// https://docs.sentry.io/platforms/javascript/guides/nextjs/
+
+import * as Sentry from "@sentry/nextjs";
+
+Sentry.init({
+  dsn:
+    "https://731a736afaef9acb39d43d1a44988fb3@o4506362098614272.ingest.sentry.io/4506362102087680",
+
+  // Adjust this value in production, or use tracesSampler for greater control
+  tracesSampleRate: 1,
+
+  // Setting this option to true will print useful information to the console while you're setting up Sentry.
+  debug: false
+});
