76ff54dc428e1e1ae9010bc754d0ed0744220a80
Johnson Ubaezuonu <jaycodist@gmail.com>
2023-12-08 01:39:57 +0100

    fixed bug with failing 404

--- next.config.js
+++ next.config.js
@@ -4,7 +4,12 @@ module.exports = {
     domains: ["storage.googleapis.com"],
     unoptimized: true
   },
-  trailingSlash: true
+  trailingSlash: true,
+  exportPathMap() {
+    return {
+      "/404": { page: "/404" }
+    };
+  }
   // i18n: {
   //   locales: ["en"],
   //   defaultLocale: "en"
