7b0e0ad1d871195d2de66b92a7d3f80eac7251e0
Taiwobello <bellotaiwolateef1999@gmail.com>
2024-01-19 17:19:52 +0100

    add base url

--- components/meta/Meta.tsx
+++ components/meta/Meta.tsx
@@ -36,6 +36,8 @@ const Meta = ({
         }`}
       />
 
+      <base href="https://floralhub.ng/" />
+
       {/* Open Graph Meta Tags */}
       {title && <meta property="og:title" content={title} />}
       {description && <meta property="og:description" content={description} />}
